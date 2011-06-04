87436a621be82639147f74521699484a08704716
Brad Beglin <brad.beglin@gatech.edu>
2011-06-04 05:54:24 -0400

    Email now works.
    On branch master
    Changes to be committed:
    	modified:   server.rb

--- server/server.rb
+++ server/server.rb
@@ -8,12 +8,15 @@ require 'builder'
 require 'twiliolib'
 require 'yajl'
 
+require 'net/smtp'
+
 API_VERSION = '2010-04-01'
 ACCOUNT_SID = 'AC811c26b1ded5a7246241880f9ec98334'
 ACCOUNT_TOKEN = 'cef36b0b4a8d4eb7429178443bb3a6d0'
 CALLER_ID = '+14155992671' #'+14043850750'
 
 get "/" do
+=begin
   t = {
     'From' => CALLER_ID,
     'To' => "4043850750",			
@@ -29,9 +32,38 @@ get "/" do
   end
   resp.error! unless resp.kind_of? Net::HTTPSuccess
   puts "code: %s\nbody: %s" % [resp.code, resp.body]
+=end
+
+=begin
+from = 'messagecarrier@lavabit.com'
+to = 'brad.beglin@gatech.edu'
+smtp_host   = 'lavabit.com'
+smtp_port   = 25
+smtp_domain = 'lavabit.com'
+smtp_user   = 'messagecarrier'
+smtp_pwd    = 'rh0kATL'
+
+subject = '[MessageCarrier] Test Email'
+emailbody = "Hello World.\n"
+time = Time.now
+emaildate = time.strftime("%a, %d %b %Y %H:%M:%S -0400")
+
+#Compose the message for the email
+emailmsg = <<END_OF_MESSAGE
+Date: #{emaildate}
+From: #{from}
+To: #{to}
+Subject: #{subject}
+  
+#{emailbody}
+END_OF_MESSAGE
+
+Net::SMTP.start(smtp_host, smtp_port, smtp_domain, smtp_user, smtp_pwd, :plain) do |smtp|
+  smtp.send_message emailmsg, from, to
+end
+=end  
   
 end
-
 
 post "/messages" do
   content_type :json
