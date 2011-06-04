f52e04aa78e1559e51385e6f819bb0c418f08fcc
Brad Beglin <brad.beglin@gatech.edu>
2011-06-04 15:49:32 -0400

    Sanitized passwords and auth keys from files.
    On branch master
    Changes to be committed:
    	modified:   server.rb

--- server/server.rb
+++ server/server.rb
@@ -11,15 +11,15 @@ require 'net/smtp'
 require 'twitter'
 
 API_VERSION = '2010-04-01'
-ACCOUNT_SID = 'AC811c26b1ded5a7246241880f9ec98334'
-ACCOUNT_TOKEN = 'cef36b0b4a8d4eb7429178443bb3a6d0'
-CALLER_ID = '+14155992671' #'+14043850750'
+ACCOUNT_SID = ''
+ACCOUNT_TOKEN = ''
+CALLER_ID = ''
 
 Twitter.configure do |config|
-  config.consumer_key = 'S11BNOKK1uwsTFFmfTYA'
-  config.consumer_secret = 'z29Ttv1OmBH0Qvo0mI6C3wLn4787lldjLMWGKdiDQ'
-  config.oauth_token = '310774926-mr98j32Iq9TxHsPttnNlQsM17B0svm3oSbDcRYQo'
-  config.oauth_token_secret = 'MIlROjzxOTulvZszM3WP9l05aE43GnoIAigRSupmhLA'
+  config.consumer_key = ''
+  config.consumer_secret = ''
+  config.oauth_token = ''
+  config.oauth_token_secret = ''
 end
 
 
@@ -94,8 +94,8 @@ helpers do
     smtp_host   = 'lavabit.com'
     smtp_port   = 25
     smtp_domain = 'lavabit.com'
-    smtp_user   = 'messagecarrier'
-    smtp_pwd    = 'rh0kATL'
+    smtp_user   = ''
+    smtp_pwd    = ''
     
     subject = "[MessageCarrier] Emergency Message"
     time = Time.now
