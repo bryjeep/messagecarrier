70330db9e84329fb43c7c0b27ab9d1c5e4a9cdc8
Brad Beglin <brad.beglin@gatech.edu>
2011-06-04 02:38:26 -0400

    First attempt at pushing SMS code.
     On branch master
     Changes to be committed:
    	modified:   README.txt
    	modified:   server.rb

--- server/server.rb
+++ server/server.rb
@@ -4,10 +4,27 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require './database'
 
+require 'twiliolib'
 require 'yajl'
 
+API_VERSION = '2010-04-01'
+ACCOUNT_SID = 'AC811c26b1ded5a7246241880f9ec98334'
+ACCOUNT_TOKEN = 'cef36b0b4a8d4eb7429178443bb3a6d0'
+CALLER_ID = '+14043850750';
+account = Twilio::RestAccount.new(ACCOUNT_SID, ACCOUNT_TOKEN)
+
 get "/" do
-  erb :index
+  t = {
+    'From' => CALLER_ID,
+    'To' => "404-909-1495",			
+    'Body' => "Hello Me..."
+  }
+  resp = account.request("/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/SMS/Messages",
+             'POST',
+             t)
+  resp.error! unless resp.kind_of? Net::HTTPSuccess
+  puts "code: %s\nbody: %s" % [resp.code, resp.body]
+  
 end
 
 
@@ -20,20 +37,21 @@ post "/messages" do
   end
   messages.each do |msg|
     begin
-      if database[:messages].where(:id => msg['id']).first
-        statuses[msg['id']] = :existing
+      if database[:messages].where(:messageid => msg['messageid']).first
+        statuses[msg['messageid']] = :existing
       else
         if database[:messages].insert(msg)
-          statuses[msg['id']] = :accepted
+          statuses[msg['messageid']] = :accepted
         else
-          statuses[msg['id']] = :error
+          statuses[msg['messageid']] = :error
         end
       end
     rescue Exception => e
-      statuses[msg['id']] = :error
+      statuses[msg['messageid']] = :error
     end
   end
   Yajl::Encoder.encode(statuses)
+
 end
 
 helpers do
